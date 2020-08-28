d7d32d639160309a82340f30768fe46e1d95b16a
Anton Boutkam <anton@nui-boutkam.nl>
2020-08-28 12:58:21 +0200

    Added server_admin to site settings

---
+++ site-settings.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'config_dir'  => 'novum.burger',
+    'namespace'   => 'NovumBurger',
+    'protocol'    => isset($_SERVER['IS_DEVEL']) ? 'http' : 'https',
+    'live_domain' => 'burger.demo.novum.nu',
+    'dev_domain'  => 'burger.demo.novum.nuidev.nl',
+    'test_domain' => 'burger.test.demo.novum.nu',
+];
