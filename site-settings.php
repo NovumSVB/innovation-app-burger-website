15cc29c00ea10bb5c36058bddb89d4ccddca161b
Anton Boutkam <anton@nui-boutkam.nl>
2020-12-04 15:57:03 +0100

    [+] Renamed composer.json [+] Removed the word API from the DNS name

--- site-settings.php
+++ site-settings.php
@@ -5,6 +5,6 @@ return [
     'namespace'   => 'NovumBurger',
     'protocol'    => isset($_SERVER['IS_DEVEL']) ? 'http' : 'https',
     'live_domain' => 'burger.demo.novum.nu',
-    'dev_domain'  => 'burger.demo.novum.nuidev.nl',
+    'dev_domain'  => 'burger.innovatieapp.nl',
     'test_domain' => 'burger.test.demo.novum.nu',
 ];
